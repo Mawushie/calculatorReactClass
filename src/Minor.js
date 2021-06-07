ee3e1c7051a2d0af94d24bf8ed505435f788ca66
Mawushie <afi.mawushie.honyah@gmail.com>
2021-06-07 10:41:23 +0000

    added responsiveness

--- src/Minor.js
+++ src/Minor.js
@@ -10,19 +10,19 @@ class Minor extends React.Component{
     render(){
         return(
             <div class="row text-center">
-                        <div class="col-md-6 lightgraycols">
+                        <div class="col-md-6 col-sm-6 col-xs-6 lightgraycols">
                             <h2 getInputVal = {this.props.getInputVal}  onClick = {this.handleOnClick}>
                                 0
                             </h2>
                         </div>
 
-                        <div class="col-md-3 lightgraycols">
+                        <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                             <h2 getInputVal = {this.props.getInputVal}  onClick = {this.handleOnClick}>
                                 .
                             </h2>
                         </div>
 
-                        <div class="col-md-3 orangecols">
+                        <div class="col-md-3 col-sm-3 col-xs-3 orangecols">
                             <h2 getInputVal = {this.props.getInputVal}  onClick = {this.handleOnClick}>
                                 =
                             </h2>
