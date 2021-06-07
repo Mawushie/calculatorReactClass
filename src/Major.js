67dc00d4ba4a376877d004afee6f73b0e4100b15
Mawushie <afi.mawushie.honyah@gmail.com>
2021-06-07 10:22:45 +0000

    added responsiveness

--- src/Major.js
+++ src/Major.js
@@ -16,79 +16,79 @@ class Major extends React.Component{
         return(
             <div class="row text-center">
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2   getInputVal = {this.props.getInputVal}  onClick = {this.handleOnClick}>
                         Reset
                     </h2>
                 </div>
                
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}> 
                         Back
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3  lightgraycols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         % 
                     </h2>
                 </div>
 
-                <div class="col-md-3 orangecols">
+                <div class="col-md-3  col-sm-3 col-xs-3 orangecols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}> 
                         ÷ 
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3  col-sm-3 col-xs-3 lightgraycols">
                     <h2   getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         7
                     </h2>
                 </div>
                
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         8
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         9
                     </h2>
                 </div>
 
-                <div class="col-md-3 orangecols">
+                <div class="col-md-3 col-sm-3 col-xs-3 orangecols">
                     <h2 getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         *
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2   getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         4
                     </h2>
                 </div>
                
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2  getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         5
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2 getInputVal = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         6
                     </h2>
                 </div>
 
-                <div class="col-md-3 orangecols">
+                <div class="col-md-3 col-sm-3 col-xs-3 orangecols">
                     <h2 onClick = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         -
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3  lightgraycols">
                     <h2  onClick = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         1
                     </h2>
@@ -100,13 +100,13 @@ class Major extends React.Component{
                     </h2>
                 </div>
 
-                <div class="col-md-3 lightgraycols">
+                <div class="col-md-3 col-sm-3 col-xs-3 lightgraycols">
                     <h2 onClick = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         3
                     </h2>
                 </div>
 
-                <div class="col-md-3 orangecols">
+                <div class="col-md-3 col-sm-3 col-xs-3 orangecols">
                     <h2 onClick = {this.props.getInputVal} onClick = {this.handleOnClick}>
                         +
                     </h2>
